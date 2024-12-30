92322627b4ab3b1067813b5ada9a8fe57cf7d5ec
Praveen S P <95535247+PraveenSP12@users.noreply.github.com>
2024-12-31 01:34:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
+require('dotenv').config();  
 const express = require('express');  
 const mysql = require('mysql2');  
 const cors = require('cors');  
-
+  
 const app = express();  
 app.use(cors());  
 app.use(express.json());  
-
+  
 const db = mysql.createConnection({  
-  host: 'bypc3fjlameol0tq2w3b-mysql.services.clever-cloud.com',  
-  user: 'unnwhnhfejgbxnt8',  
-  password: '4KZXeNuoZcg7OAOeOfjQ',  
-  database: 'bypc3fjlameol0tq2w3b',  
+  host: process.env.DB_HOST,  
+  user: process.env.DB_USER,  
+  password: process.env.DB_PASSWORD,  
+  database: process.env.DB_NAME,  
 });  
-
+  
 db.connect(err => {  
   if (err) throw err;  
   console.log('Connected to MySQL');  
 });  
-
+  
 app.post('/add-patient', (req, res) => {  
   const patient = req.body;  
   const sql = 'INSERT INTO patients SET ?';  
@@ -26,18 +27,18 @@ app.post('/add-patient', (req, res) => {
     res.send('Patient added');  
   });  
 });  
-
+  
 app.get('/patients', (req, res) => {  
-    const sql = 'SELECT * FROM patients';  
-    db.query(sql, (err, result) => {  
-      if (err) {  
-        console.error('Error fetching patients:', err);  
-        res.status(500).send('Error fetching patients');  
-        return;  
-      }  
-      res.json(result);  
-    });  
+  const sql = 'SELECT * FROM patients';  
+  db.query(sql, (err, result) => {  
+    if (err) {  
+      console.error('Error fetching patients:', err);  
+      res.status(500).send('Error fetching patients');  
+      return;  
+    }  
+    res.json(result);  
   });  
+});  
   
 const PORT = process.env.PORT || 5000;  
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
